b0bab267a91f91e326446365a724b5989b018e10
Gyanendra <gyaanendrap@gmail.com>
2025-07-03 20:01:39 +0530

    fixed navbar error

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,5 +1,4 @@
 import { Dock, DockIcon } from "@/components/magicui/dock";
-import { ModeToggle } from "@/components/mode-toggle";
 import { buttonVariants } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import {
